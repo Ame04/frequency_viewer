49e45bc8ca6a1254d3bf49eb2b2d41b3a5951a7e
Camus Armand <camus.armand@gmail.com>
2021-10-27 18:43:55 +0200

    ADDED: the implementation of the refresh fonction in src/headers/led_gestion.c and changed the data representation of the leds

--- src/codes/led_gestion.c
+++ src/codes/led_gestion.c
@@ -12,14 +12,42 @@
 #include "../headers/led_gestion.h"
 
 void init_GPIO(){
-   
+   const uint gpioToInit[]={GPIO_TO_INIT};
+   int nbLedInit=0;
+   for(int i=0 ; i<(NB_LED_COLUMN+1) ; ++i)
+   {
+       for(int j=0; j<NB_LED_PER_COLUMN ; ++j)
+       {
+           Led currentLed=ledMatix[i][j];
+           if(nbLedInit<NB_GPIO_TO_INIT)
+           {
+               currentLed.gpio=gpioToInit[nbLedInit];
+               gpio_init(currentLed.gpio);
+               gpio_set_dir(currentLed.gpio,GPIO_OUT);
+               gpio_put(currentLed.gpio,false);
+           }
+           else
+           {
+               currentLed.gpio=0;
+           }
+           currentLed.state=false;
+           nbLedInit++;
+       }
+   }
     
 }
 
 void refresh_led_from_amplitude(uint nbColumnToModify, uint* tabOfValue){
-// map on 5 for both values
+// map on 5 for all the values, uses NB_AMPLITUDE_VALUE to map
     for(int i =0 ; i<nbColumnToModify ; ++i){
-        tabOfValue[i] = (int)(tabOfValue[i]/NB_AMPLITUDE_VALUES*NB_LED_PER_COLUMN);
+        int futureValue = (int)(tabOfValue[i]/NB_AMPLITUDE_VALUES*NB_LED_PER_COLUMN);
+        if(futureValue==5)
+        {
+            tabOfValue[i]=4;
+        }
+        else{
+            tabOfValue[i]=futureValue;
+        }
     }
 
     refresh_led(nbColumnToModify, tabOfValue);
@@ -39,11 +67,23 @@ void refresh_led(uint nbColumnToModify, uint* tabOfValue){
         }
     }
 
-    for(i =0;i<NB_LED_COLUMN;++i){}
     int difference = 0;
-
-
-
-
+    int sign=0;
+    for(i =0;i<NB_LED_COLUMN;++i)
+    {
+        difference = tabOfValue[i]-oldColumnValues[i];
+        if(difference)
+        {
+            sign=difference/abs(difference);
+            for (int j = oldColumnValues[i]; i!=tabOfValue[i]; i+=sign)
+            {
+                ledMatix[i][j].state=!ledMatix[i][j].state;
+                gpio_put(ledMatix[i][j].gpio,ledMatix[i][j].state);
+            }
+            
+            
+        }
+    }
+    
 
 }
--- src/headers/led_gestion.h
+++ src/headers/led_gestion.h
@@ -14,23 +14,64 @@
 #define LED_GESTION_H
 
     #include "hardware/gpio.h"
+    #include <stdlib.h>
 
+    /**
+    * \def NB_AMPLITUDE_VALUES
+    * \brief The number of value that can be returned by the adc
+    */
     #define NB_AMPLITUDE_VALUES 4096 //for a 12bits ADC
+
+    /**
+    * \def NB_LED_PER_COLUMN
+    * \brief The max number of led per column
+    */
     #define NB_LED_PER_COLUMN 5
+
+    /**
+    * \def NB_LED_COLUMN
+    * \brief The number of led column in the matrix
+    */
     #define NB_LED_COLUMN 2
 
-    const uint GPIO_TO_INIT[]={0,1,2,3,4,5,6,7,8,9,10};
-    const uint NB_GPIO_TO_INIT=11;
+    /**
+    * \def GPIO_TO_INIT
+    * \brief All the Gpio to init, they have to be separated by comas as they will be transfered dirrectly in a tab. You can not use the GPIO 0.
+    */
+    #define GPIO_TO_INIT 1,2,3,4,5,6,7,8,9,10,11
+
+    /**
+    * \def NB_GPIO_TO_INIT
+    * \brief The number of Gpio to init
+    */
+    #define NB_GPIO_TO_INIT 11
+
+    /**
+    * \struct Led
+    * \brief Represetn a led in the system
+    */
+    typedef struct Led
+    {
+        uint gpio;
+        bool state;
+    }Led;
+    
+
+    Led ledMatix[NB_LED_COLUMN+1][NB_LED_PER_COLUMN];
 
     /**
      * \fn void init_GPIO()
-     * \brief initialyses the GPIO for the leds
+     * \brief Initialyses the GPIO for the leds and fill the ledMatrix.
+     * 
+     *  The ledMatrix will be initialised column by column with the gpios given in GPIO_TO_INIT. It will take them in the natural order utile it's full.
+     * If there is not enough places in the matrix the left leds will be ignored. If there is not enough leds to fill the matrix, 
+     * it will fill the end with led on the gpio 0 wich can't be chosen as part of the GPIO_TO_INIT.
      */ 
     void init_GPIO();
 
     /**
      * \fn void refresh_led_from_amplitude(uint nbColumnToModify, uint* tabOfValue)
-     * \brief maps the amplitude value to give it to refresh_led
+     * \brief Maps the amplitude value to give it to refresh_led
      * 
      * \param nbColumnToModify the size of the given tab
      * \param tabOfValue A tap with the value to map on each column of LED
@@ -39,7 +80,7 @@
 
     /**
      * \fn void refresh_led(uint nbColumnToModify, uint* tabOfValue)
-     * \brief change the led display
+     * \brief Change the led display
      * 
      * \param nbColumnToModify the size of the given tab
      * \param tabOfValue A tap with the value to map on each column of LED
